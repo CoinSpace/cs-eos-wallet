ad50755fbdee011e414e19bffc4515a9c380d8b4
Evgeny Vlasenko <mahnunchik@gmail.com>
2021-01-28 15:51:02 +0200

    chore: update eslint, refactor

--- test/index.js
+++ test/index.js
@@ -1,37 +1,37 @@
 'use strict';
 
-var assert = require('assert');
-var Wallet = require('../');
-var fixtures = require('./wallet');
+const assert = require('assert');
+const Wallet = require('../');
+const fixtures = require('./wallet');
 // eslint-disable-next-line max-len
-var RANDOM_SEED = '2b48a48a752f6c49772bf97205660411cd2163fe6ce2de19537e9c94d3648c85c0d7f405660c20253115aaf1799b1c41cdd62b4cfbb6845bc9475495fc64b874';
-var RANDOM_SEED_PUB_KEY = 'EOS7tJKsK8frEPribVBiQXByLkADnDUr3DUUr4LBzuThFPYk8EPSj';
+const RANDOM_SEED = '2b48a48a752f6c49772bf97205660411cd2163fe6ce2de19537e9c94d3648c85c0d7f405660c20253115aaf1799b1c41cdd62b4cfbb6845bc9475495fc64b874';
+const RANDOM_SEED_PUB_KEY = 'EOS7tJKsK8frEPribVBiQXByLkADnDUr3DUUr4LBzuThFPYk8EPSj';
 
-describe('EOS Wallet', function() {
-  var readOnlyWallet;
+describe('EOS Wallet', () => {
+  let readOnlyWallet;
 
-  before(function() {
+  before(() => {
     readOnlyWallet = Wallet.deserialize(JSON.stringify(fixtures));
   });
 
-  it('should have more tests', function() {
+  it('should have more tests', () => {
     assert.equal('hi', 'hi');
   });
 
-  describe('constructor', function() {
-    it('with seed', function() {
-      var wallet = new Wallet({
+  describe('constructor', () => {
+    it('with seed', () => {
+      const wallet = new Wallet({
         networkName: 'eos',
-        seed: RANDOM_SEED
+        seed: RANDOM_SEED,
       });
       assert.ok(wallet);
       assert.equal(wallet.isLocked, false);
     });
 
-    it('with publicKey', function() {
-      var wallet = new Wallet({
+    it('with publicKey', () => {
+      const wallet = new Wallet({
         networkName: 'eos',
-        publicKey: readOnlyWallet.account.owner.publicKey
+        publicKey: readOnlyWallet.account.owner.publicKey,
       });
       assert.equal(wallet.account.owner.publicKey, readOnlyWallet.account.owner.publicKey);
       assert.equal(wallet.account.active.publicKey, readOnlyWallet.account.active.publicKey);
@@ -40,11 +40,11 @@ describe('EOS Wallet', function() {
     });
   });
 
-  describe('lock', function() {
-    it('works', function() {
-      var wallet = new Wallet({
+  describe('lock', () => {
+    it('works', () => {
+      const wallet = new Wallet({
         networkName: 'eos',
-        seed: RANDOM_SEED
+        seed: RANDOM_SEED,
       });
       assert.equal(wallet.isLocked, false);
       wallet.lock();
@@ -55,11 +55,11 @@ describe('EOS Wallet', function() {
     });
   });
 
-  describe('unlock', function() {
-    it('works', function() {
-      var wallet = new Wallet({
+  describe('unlock', () => {
+    it('works', () => {
+      const wallet = new Wallet({
         networkName: 'eos',
-        publicKey: RANDOM_SEED_PUB_KEY
+        publicKey: RANDOM_SEED_PUB_KEY,
       });
       assert.equal(wallet.isLocked, true);
       wallet.unlock(RANDOM_SEED);
@@ -72,46 +72,46 @@ describe('EOS Wallet', function() {
     });
   });
 
-  describe('publicKey', function() {
-    it('works', function() {
-      var wallet = new Wallet({
+  describe('publicKey', () => {
+    it('works', () => {
+      const wallet = new Wallet({
         networkName: 'eos',
-        seed: RANDOM_SEED
+        seed: RANDOM_SEED,
       });
-      var publicKey = wallet.publicKey();
+      const publicKey = wallet.publicKey();
       assert.ok(publicKey);
     });
 
-    it('key is valid', function() {
-      var wallet = new Wallet({
+    it('key is valid', () => {
+      const wallet = new Wallet({
         networkName: 'eos',
-        seed: RANDOM_SEED
+        seed: RANDOM_SEED,
       });
-      var publicKey = wallet.publicKey();
-      var secondWalet = new Wallet({
+      const publicKey = wallet.publicKey();
+      const secondWalet = new Wallet({
         networkName: 'eos',
-        publicKey: publicKey
+        publicKey,
       });
       secondWalet.unlock(RANDOM_SEED);
 
-      var secondAccount = secondWalet.account;
+      const secondAccount = secondWalet.account;
       assert.equal(wallet.account.owner.privateKey, secondAccount.owner.privateKey);
       assert.equal(wallet.account.active.privateKey, secondAccount.active.privateKey);
       assert.equal(wallet.account.signatureProvider.availableKeys[0], secondAccount.signatureProvider.availableKeys[0]);
     });
   });
 
-  describe('serialization & deserialization', function() {
-    it('works', function() {
+  describe('serialization & deserialization', () => {
+    it('works', () => {
       assert.deepEqual(fixtures, JSON.parse(readOnlyWallet.serialize()));
     });
   });
 
-  describe('exportPrivateKeys', function() {
-    it('works', function() {
-      var csv = readOnlyWallet.exportPrivateKeys();
+  describe('exportPrivateKeys', () => {
+    it('works', () => {
+      const csv = readOnlyWallet.exportPrivateKeys();
       assert.equal(typeof csv, 'string');
-      var str = 'ownerPrivateKey,ownerPublicKey,activePrivateKey,activePublicKey\n';
+      let str = 'ownerPrivateKey,ownerPublicKey,activePrivateKey,activePublicKey\n';
       str += readOnlyWallet.account.owner.privateKey + ',' + readOnlyWallet.account.owner.publicKey + ',';
       str += readOnlyWallet.account.active.privateKey + ',' + readOnlyWallet.account.active.publicKey;
       assert.equal(csv, str);
