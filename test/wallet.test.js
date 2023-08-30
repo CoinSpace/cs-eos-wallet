25af10b5dd8b2974beac76b5a4eefe93792cc35f
Evgeny Vlasenko <mahnunchik@gmail.com>
2023-08-30 15:14:24 -0600

    fix: error on inactive account

--- test/wallet.test.js
+++ test/wallet.test.js
@@ -514,9 +514,9 @@ describe('EOS Wallet', () => {
             amount: new Amount(123n, wallet.crypto.decimals),
           });
         }, {
-          name: 'SmallAmountError',
-          message: 'Small amount',
-          amount: new Amount(1n, wallet.crypto.decimals),
+          name: 'BigAmountError',
+          message: 'Big amount',
+          amount: new Amount(0n, wallet.crypto.decimals),
         });
       });
 
