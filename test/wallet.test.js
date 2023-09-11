0cdae8d7a1deea78c8caf331a8b654c1da16e61b
Evgeny Vlasenko <mahnunchik@gmail.com>
2023-09-11 14:33:13 -0600

    fix: seed encoding issue

--- test/wallet.test.js
+++ test/wallet.test.js
@@ -3,8 +3,10 @@ import Wallet from '@coinspace/cs-eos-wallet';
 import assert from 'assert/strict';
 import sinon from 'sinon';
 
+import { hexToBytes } from '@noble/hashes/utils';
+
 // eslint-disable-next-line max-len
-const RANDOM_SEED = Buffer.from('2b48a48a752f6c49772bf97205660411cd2163fe6ce2de19537e9c94d3648c85c0d7f405660c20253115aaf1799b1c41cdd62b4cfbb6845bc9475495fc64b874', 'hex');
+const RANDOM_SEED = hexToBytes('2b48a48a752f6c49772bf97205660411cd2163fe6ce2de19537e9c94d3648c85c0d7f405660c20253115aaf1799b1c41cdd62b4cfbb6845bc9475495fc64b874');
 const PUBLIC_KEY = 'EOS7tJKsK8frEPribVBiQXByLkADnDUr3DUUr4LBzuThFPYk8EPSj';
 const PRIVATE_KEY = '5J31TthDctkYwYVrDTcg8JmjmbK58UFzyPHBy9bzd5XFz2JKswJ';
 const ACCOUNT_NAME = 'zxczxczxczxc';
